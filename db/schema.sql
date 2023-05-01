97b0d22a8d50a7671e03a5bb0fcee1e339334f1f
radhey-molsoft <himali.baps@gmail.com>
2023-05-01 15:06:33 -0400

    role table added

--- db/schema.sql
+++ db/schema.sql
@@ -11,3 +11,13 @@ CREATE TABLE department (
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Role Table
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
