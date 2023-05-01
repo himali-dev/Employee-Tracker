91cbaca2e7130dbdfb41ea18c97c9dfd60d5e6f7
radhey-molsoft <himali.baps@gmail.com>
2023-05-01 15:06:09 -0400

    dept table added

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,13 @@
 -- Database Creation
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
+
+-- Pointing to employees database
+USE employees;
+
+-- Department Table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
