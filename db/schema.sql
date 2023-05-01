25a80b9aeb9c76ecd6a2b2d305257f8313574bf3
radhey-molsoft <himali.baps@gmail.com>
2023-05-01 15:06:53 -0400

    emp table added

--- db/schema.sql
+++ db/schema.sql
@@ -21,3 +21,15 @@ CREATE TABLE role (
     PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+-- Employee Table
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
