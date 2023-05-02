c461ea203366bdfdb6402807c0d464f0b3c6795a
radhey-molsoft <himali.baps@gmail.com>
2023-05-02 18:03:04 -0400

    start menu added

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
 // call the reuqired modules
 const inquirer = require("inquirer");
 const connection = require("./db/connection.js");
+require("console.table");
+
+const homePage = () => {
+
+inquirer.prompt({
+  name: "first",
+  type: "list",
+  message: "What would you like to do",
+  choices: [
+    "View Departments",
+    "View Roles",
+    "View Employees",
+    "View Employees By Manager",
+    "View Employees By Department",
+    "View Total Utilized Budget for Department",
+    "Update Employee Role",
+    "Update Employee Manager",
+    "Add Department",
+    "Add Role",
+    "Add Employee",
+    "Remove Employee",
+    "Remove Department",
+    "Remove Role",
+    "Exit"
+  ]
+})
+
+
+
+}
