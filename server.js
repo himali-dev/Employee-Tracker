b9947488c56692f379dc0318c5d14bb835ed08e2
radhey-molsoft <himali.baps@gmail.com>
2023-05-02 19:40:32 -0400

    view department function added

--- server.js
+++ server.js
@@ -50,14 +50,50 @@ inquirer.prompt({
       View_Employees_By_Department();
       break;
 
+    case "View Total Utilized Budget for Department":
+      View_Total_Utilized_Budget_for_Department();
+      break;
+
+    case "Update Employee Role":
+      Update_Employee_Role();
+
+    case "Update Employee Manager":
+      Update_Employee_Manager();
+
+    case "Add Department":
+      Add_Department();
+
+    case "Add Role":
+      Add_Role();
+
+    case "Add Employee":
+      Add_Employee();
+
+    case "Remove Employee":
+      Remove_Employee();
+
+    case "Remove Department":
+      Remove_Department();
+
+    case "Remove Role":
+      Remove_Role();
+
     case "Exit":
       Exit();
       break;
   }
 });
+};
 
-
-
-
-
+//view departments
+function View_Departments() {
+  const query = `SELECT
+  department.id,
+  department.name FROM
+  department;`;
+  connection.query(query, (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    homePage();
+  });
 }
